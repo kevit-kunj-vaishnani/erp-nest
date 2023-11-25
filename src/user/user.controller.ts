4252d42c48389f74bf39f887736ecad7faecb576
kevit-kunj-vaishnani <kevit.kunj.vaishnani@gmail.com>
2023-11-25 16:14:43 +0530

    auth add in user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,14 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Post, Get, Param, Patch, Delete, Session } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, Patch, Delete, Session, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
 import { UpdateSelfDto } from './dto/update-self.dto';
+import { AdminGuard } from 'src/guard/admin.guard';
+import { AuthGuard } from 'src/guard/auth.gaurd';
+import { StaffAdminGuard } from 'src/guard/staff-admin.guard';
 
 @Controller('user')
 export class UserController {
@@ -13,31 +16,38 @@ export class UserController {
     constructor(private userService: UserService, private authService: AuthService){}
 
     @Post('/add')
-    async addUser(@Body() user:CreateUserDto, @Session() session:any) {
+    @UseGuards(AuthGuard)
+    @UseGuards(AdminGuard)
+    async addUser(@Body() user:CreateUserDto) {
 
         const userD = await this.authService.addUser(user);
-        
-        session.userId = userD._id;   
         
         return userD;
     }
 
     @Get()
+    @UseGuards(AuthGuard)
+    @UseGuards(AdminGuard)
     async findUsers() {
         return await this.userService.findAllUser();
     }
     
+    @UseGuards(AuthGuard)
+    @UseGuards(StaffAdminGuard)
     @Patch('/update/me')
     updateSelf(@Body() body: UpdateSelfDto, @Session() session: any) {
         return this.userService.findOneUserAndUpdate(session.userid, body);
     }
 
+    @UseGuards(AuthGuard)
+    @UseGuards(AdminGuard)
     @Patch('/update/:_id')
     getUserByIdAndUpdate(@Param('_id') _id: string, @Body() body: UpdateUserDto){
         return this.userService.findOneUserAndUpdate(_id,body)
     }
 
-
+    @UseGuards(AuthGuard)
+    @UseGuards(AdminGuard)
     @Delete('/delete/:_id')
     getUserByIdAndDelete(@Param('_id') _id: string){
         return this.userService.findOneUserByIdAndDelete(_id)
@@ -45,19 +55,24 @@ export class UserController {
 
     @Post('/login')
     async signIn(@Body() body: LoginUserDto, @Session() session: any) {
-      const user = await this.authService.login(body.email, body.password);
       
+      const user = await this.authService.login(body.email, body.password);
+
       session.userid = user._id;
       session.role = user.role;
-    
+      
       return user;
     }
 
+    @UseGuards(AuthGuard)
+    @UseGuards(StaffAdminGuard)
     @Get('/self')
     async me(@Session() session: any){
         return await this.userService.findUser(session.userid);
     }
 
+    @UseGuards(AuthGuard)
+    @UseGuards(StaffAdminGuard)
     @Post('/logout')
     async logout(@Session() session:any){
 
